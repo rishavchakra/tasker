f44673954058f5f6d09a36459fcfb5e61784bffa
rishavchakra <rishavchakra@gmail.com>
2021-08-14 13:27:56 -0500

    GTK initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use gio::prelude::*;
+use gtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let app = gtk::Application::new(
+        Some("com.rishavc.tasker_gui"),
+        gio::ApplicationFlags::FLAGS_NONE,
+    ).expect("Failed to initialize GTK");
+
+    app.connect_activate(|app| {
+        let window = gtk::ApplicationWindow::new(app);
+        window.set_title("Tasker");
+        window.set_default_size(400, 240);
+        window.show_all();
+    });
+
+    app.run(&std::env::args().collect::<Vec<_>>());
 }
