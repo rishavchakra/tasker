586a59e871fec35a48c639b4615d1456a504eedd
rishavchakra <rishavchakra@gmail.com>
2021-08-19 16:09:58 -0500

    JSON retrieval functionality

--- src/data.rs
+++ src/data.rs
@@ -1,8 +1,10 @@
 // This module contains code for retrieving and saving data
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
-use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
 
 // At the top level, there is a list of boards
 // Within each board, there is a list of Categories
@@ -16,21 +18,31 @@ use std::collections::HashMap;
 // Boards contain categories, which contain items
 // Easier to organize but harder to work with
 
-struct _Board {
-    name: String,
-    description: String,
-    categories: Vec<_Category>
+// Or, use a simpler organization
+// Tasks are stored in a top-level list
+// Tasks store the category and board they are under
+// Boards are also stored in the top-level and contain category names
+
+#[derive(Serialize, Deserialize)]
+pub struct TaskData {
+    boards: Vec<Board>,
+    tasks: Vec<Task>,
 }
 
-struct _Category {
-    name: String,
-    items: Vec<Item>
-}
-
-struct Item {
+#[derive(Serialize, Deserialize)]
+struct Board {
     name: String,
     description: String,
-    state: bool
+    categories: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Task {
+    name: String,
+    description: String,
+    state: bool,
+    board: usize,
+    category: usize
 }
 
 pub fn add_board(_name: String) {
@@ -41,21 +53,94 @@ pub fn add_category(_name: String) {
     // TODO: also this
 }
 
-pub fn add_item(_name: String, _description: String) {
+pub fn add_task(_name: String, _description: String) {
     // TODO: this too
-    let item_to_add = Item {
-        name: _name,
-        description: _description,
-        state: false
-    };
+    // let task_to_add = Task {
+    //     name: _name,
+    //     description: _description,
+    //     state: false,
+    // };
 }
 
-pub fn open_json() {
-    let mut data_file = File::open("tasker_data.json")
-        .expect("ERROR: Could not find the data json file");
+pub fn get_board_names() -> Vec<String> {
+    let data = open_json();
+    data.boards.iter().map(|board| board.name.clone()).collect::<Vec<String>>()
+}
+
+pub fn get_category_names(board_name: &String) -> Option<Vec<String>> {
+    let data = open_json();
+    for board in &data.boards {
+        if &board.name == board_name {
+            return Some(board.categories.clone());
+        }
+    }
+    None
+}
+
+pub fn get_category_names_ind(board: usize) -> Option<Vec<String>> {
+    let data = open_json();
+    if board > data.boards.len() {
+        return None;
+    }
+    Some(data.boards[board].categories.clone())
+}
+
+pub fn get_task_names(board_name: &String, category_name: &String) -> Vec<String> {
+    let data = open_json();
+    let boards = get_board_names();
+    let categories = get_category_names(&board_name).unwrap();
+    let mut task_names = Vec::new();
+    for task in &data.tasks {
+        if &boards[task.board] == board_name && &categories[task.category] == category_name {
+            task_names.push(task.name.clone());
+        }
+    }
+    task_names
+}
+
+pub fn get_task_names_ind(board: usize, category: usize) -> Vec<String> {
+    let data = open_json();
+    let mut task_names = Vec::new();
+    for task in &data.tasks {
+        if board == task.board && category == task.category {
+            task_names.push(task.name.clone());
+        }
+    }
+    task_names
+}
+
+pub fn get_tasks(board_name: &String, category_name: &String) -> Vec<Task> {
+    let data = open_json();
+    let boards = get_board_names();
+    let categories = get_category_names(&board_name).unwrap();
+    let mut tasks = Vec::new();
+    for task in &data.tasks {
+        if &boards[task.board] == board_name && &categories[task.category] == category_name {
+            tasks.push(task.clone());
+        }
+    }
+    tasks
+}
+
+pub fn get_tasks_ind(board: usize, category: usize) -> Vec<Task> {
+    let data = open_json();
+    let mut tasks = Vec::new();
+    for task in &data.tasks {
+        if board == task.board && category == task.category {
+            tasks.push(task.clone());
+        }
+    }
+    tasks
+}
+
+fn open_json() -> TaskData {
+    let mut data_file =
+        File::open("tasker_data.json").expect("ERROR: Could not find the data json file");
     let mut data_str = String::new();
-    data_file.read_to_string(&mut data_str)
+    data_file
+        .read_to_string(&mut data_str)
         .expect("ERROR: Could not read data file into string");
-    
-    println!("{}", data_str);
+    let json_obj: TaskData =
+        serde_json::from_str(&data_str).expect("ERROR: Could not parse JSON data");
+    json_obj
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use gio::prelude::*;
 use gtk::prelude::*;
 
 fn main() {
-    data::open_json();
     let app = gtk::Application::new(
         Some("com.rishavc.tasker_gui"),
         gio::ApplicationFlags::FLAGS_NONE,
@@ -18,4 +17,30 @@ fn main() {
     });
 
     app.run(&std::env::args().collect::<Vec<_>>());
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_json_boardnames() {
+        let board_names = data::get_board_names();
+        assert_eq!(board_names, vec!["Board0", "Board1"]);
+    }
+
+    #[test]
+    fn test_json_categorynames() {
+        let board_names = data::get_board_names();
+        let cat_name1 = data::get_category_names(&board_names[0]).unwrap();
+        assert_eq!(cat_name1, vec!["Board 0 category 0", "Board 0 category 1"]);
+        let cat_name2 = data::get_category_names(&board_names[1]).unwrap();
+        assert_eq!(cat_name2, vec!["Board 1 category 0", "Board 1 category 1"]);
+    }
+
+    #[test]
+    fn test_json_tasknames() {
+        // let task_names = data::get_task_names(&String::from("Board0"), &String::from("Category0"));
+        let task_names = data::get_task_names_ind(0, 0);
+        assert_eq!(task_names, vec!["Task name", "Another task name"]);
+    }
 }
